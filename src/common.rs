2c33085cf9587a530b0801c861487912f6896527
charli <charli.vannood@gmail.com>
2024-12-23 22:25:58 +0100

    start fixing memory leak

--- src/common.rs
+++ src/common.rs
@@ -277,19 +277,23 @@ struct State {
     text_pipeline: wgpu::RenderPipeline,
     gui_item_block_pipeline: wgpu::RenderPipeline,
     vertex_buffers: Vec<wgpu::Buffer>,
+    world_vertex_buffer: wgpu::Buffer,
     gui_vertex_buffers: Vec<wgpu::Buffer>,
     text_vertex_buffers: Vec<wgpu::Buffer>,
     gui_item_block_vertex_buffers: Vec<wgpu::Buffer>,
     uniform_bind_groups: Vec<wgpu::BindGroup>,
+    world_uniform_bind_group: wgpu::BindGroup,
     gui_uniform_bind_groups: Vec<wgpu::BindGroup>,
     text_uniform_bind_groups: Vec<wgpu::BindGroup>,
     gui_item_block_uniform_bind_groups: Vec<wgpu::BindGroup>,
     vertex_uniform_buffers: Vec<wgpu::Buffer>,
+    world_vertex_uniform_buffer: wgpu::Buffer,
     gui_vertex_uniform_buffers: Vec<wgpu::Buffer>,
     text_vertex_uniform_buffers: Vec<wgpu::Buffer>,
     gui_item_block_vertex_uniform_buffers: Vec<wgpu::Buffer>,
     project_mat: Matrix4<f32>,
     num_vertices: Vec<u32>,
+    world_num_vertices: u64,
     gui_num_vertices: Vec<u32>,
     text_num_vertices: Vec<u32>,
     gui_item_block_num_vertices: Vec<u32>,
@@ -302,127 +306,6 @@ struct State {
 }
 
 impl State {
-    fn create_object(
-        game_data: &GameData, init: &transforms::InitWgpu, light_data: Light, 
-        uniform_bind_group_layout: &wgpu::BindGroupLayout, i: usize) -> (BindGroup, wgpu::Buffer, wgpu::Buffer, u32) {
-        // create vertex uniform buffer
-        // model_mat and view_projection_mat will be stored in vertex_uniform_buffer inside the update function
-        let vertex_uniform_buffer = init.device.create_buffer(&wgpu::BufferDescriptor{
-            label: Some("Vertex Uniform Buffer"),
-            size: 192,
-            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-            mapped_at_creation: false,
-        });
-       
-        // create fragment uniform buffer. here we set eye_position = camera_position and light_position = eye_position
-        let fragment_uniform_buffer = init.device.create_buffer(&wgpu::BufferDescriptor{
-            label: Some("Fragment Uniform Buffer"),
-            size: 32,
-            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-            mapped_at_creation: false,
-        });
-
-        // store light and eye positions
-        let light_position:&[f32; 3] = &Point3::new(0.0, 128.0, 0.0).into();
-        let eye_position:&[f32; 3] = &Point3::new(0.0, 128.0, 0.0).into();
-        init.queue.write_buffer(&fragment_uniform_buffer, 0, bytemuck::cast_slice(light_position));
-        init.queue.write_buffer(&fragment_uniform_buffer, 16, bytemuck::cast_slice(eye_position));
-
-        // create light uniform buffer
-        let light_uniform_buffer = init.device.create_buffer(&wgpu::BufferDescriptor{
-            label: Some("Light Uniform Buffer"),
-            size: 48,
-            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-            mapped_at_creation: false,
-        });
-
-        // store light parameters
-        init.queue.write_buffer(&light_uniform_buffer, 0, bytemuck::cast_slice(&[light_data]));
-
-        let texture_data = Assets::get("textures/blocks/atlas.png").expect("Failed to load embedded texture");
-        let img = image::load_from_memory(&texture_data.data).expect("Failed to load texture");
-        let rgba = img.to_rgba8();
-        let (width, height) = img.dimensions();
-        let texture_size = wgpu::Extent3d {
-            width,
-            height,
-            depth_or_array_layers: 1,
-        };
-        
-        let texture = init.device.create_texture(&wgpu::TextureDescriptor {
-            label: Some("Texture"),
-            size: texture_size,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Rgba8UnormSrgb,
-            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-            view_formats: &[],
-        });
-        
-        init.queue.write_texture(
-            wgpu::ImageCopyTexture {
-                texture: &texture,
-                mip_level: 0,
-                origin: wgpu::Origin3d::ZERO,
-                aspect: wgpu::TextureAspect::All,
-            },
-            &rgba,
-            wgpu::ImageDataLayout {
-                offset: 0,
-                bytes_per_row: Some(4 * width),
-                rows_per_image: Some(height),
-            },
-            texture_size,
-        );
-        
-        let texture_view = texture.create_view(&wgpu::TextureViewDescriptor::default());
-        let sampler = init.device.create_sampler(&wgpu::SamplerDescriptor {
-            address_mode_u: wgpu::AddressMode::ClampToEdge,
-            address_mode_v: wgpu::AddressMode::ClampToEdge,
-            address_mode_w: wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Nearest,
-            min_filter: wgpu::FilterMode::Nearest,
-            mipmap_filter: wgpu::FilterMode::Nearest,
-            ..Default::default()
-        });
-
-        let uniform_bind_group = init.device.create_bind_group(&wgpu::BindGroupDescriptor{
-            layout: &uniform_bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: vertex_uniform_buffer.as_entire_binding(),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: fragment_uniform_buffer.as_entire_binding(),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 2,
-                    resource: light_uniform_buffer.as_entire_binding(),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 3,
-                    resource: wgpu::BindingResource::TextureView(&texture_view),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 4,
-                    resource: wgpu::BindingResource::Sampler(&sampler),
-                },
-            ],
-            label: Some("Uniform Bind Group"),
-        });
-
-        let vertex_buffer = init.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Vertex Buffer"),
-            contents: cast_slice(&game_data.objects[i]),
-            usage: wgpu::BufferUsages::VERTEX,
-        });
-        let num_vertices = game_data.objects[i].len() as u32;
-
-        return (uniform_bind_group, vertex_uniform_buffer, vertex_buffer, num_vertices)
-    }
     fn create_object_gui(
         game_data: &GameData, init: &transforms::InitWgpu, light_data: Light, 
         uniform_bind_group_layout: &wgpu::BindGroupLayout, i: usize) -> (BindGroup, wgpu::Buffer, wgpu::Buffer, u32) {
@@ -783,6 +666,132 @@ impl State {
             usage: wgpu::BufferUsages::VERTEX,
         });
         let num_vertices = game_data.gui_item_block_objects[i].len() as u32;
+
+        return (uniform_bind_group, vertex_uniform_buffer, vertex_buffer, num_vertices)
+    }
+    fn create_world_buffer(
+        init: &transforms::InitWgpu, light_data: Light, 
+        uniform_bind_group_layout: &wgpu::BindGroupLayout, world_data_thread: &Arc<Mutex<world::WorldData>>) -> (BindGroup, wgpu::Buffer, wgpu::Buffer, u64) {
+        // create vertex uniform buffer
+        // model_mat and view_projection_mat will be stored in vertex_uniform_buffer inside the update function
+        let vertex_uniform_buffer = init.device.create_buffer(&wgpu::BufferDescriptor{
+            label: Some("Vertex Uniform Buffer"),
+            size: 192,
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+       
+        // create fragment uniform buffer. here we set eye_position = camera_position and light_position = eye_position
+        let fragment_uniform_buffer = init.device.create_buffer(&wgpu::BufferDescriptor{
+            label: Some("Fragment Uniform Buffer"),
+            size: 32,
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        // store light and eye positions
+        let light_position:&[f32; 3] = &Point3::new(0.0, 128.0, 0.0).into();
+        let eye_position:&[f32; 3] = &Point3::new(0.0, 128.0, 0.0).into();
+        init.queue.write_buffer(&fragment_uniform_buffer, 0, bytemuck::cast_slice(light_position));
+        init.queue.write_buffer(&fragment_uniform_buffer, 16, bytemuck::cast_slice(eye_position));
+
+        // create light uniform buffer
+        let light_uniform_buffer = init.device.create_buffer(&wgpu::BufferDescriptor{
+            label: Some("Light Uniform Buffer"),
+            size: 48,
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        // store light parameters
+        init.queue.write_buffer(&light_uniform_buffer, 0, bytemuck::cast_slice(&[light_data]));
+        
+        let texture_size;
+        let rgba: Vec<u8>;
+        let width: u32;
+        let height: u32;
+
+        {
+            let world_data = world_data_thread.lock().unwrap();
+            texture_size = world_data.textures[0].1;
+            rgba = world_data.textures[0].0.to_vec();
+            width = world_data.textures[0].2;
+            height = world_data.textures[0].3;
+        }
+
+        let texture = init.device.create_texture(&wgpu::TextureDescriptor {
+            label: Some("Texture"),
+            size: texture_size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+            view_formats: &[],
+        });
+        
+        init.queue.write_texture(
+            wgpu::ImageCopyTexture {
+                texture: &texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+                aspect: wgpu::TextureAspect::All,
+            },
+            &rgba,
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: Some(4 * width),
+                rows_per_image: Some(height),
+            },
+            texture_size,
+        );
+        
+        let texture_view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let sampler = init.device.create_sampler(&wgpu::SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Nearest,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        });
+
+        let uniform_bind_group = init.device.create_bind_group(&wgpu::BindGroupDescriptor{
+            layout: &uniform_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: vertex_uniform_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: fragment_uniform_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: light_uniform_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 3,
+                    resource: wgpu::BindingResource::TextureView(&texture_view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 4,
+                    resource: wgpu::BindingResource::Sampler(&sampler),
+                },
+            ],
+            label: Some("Uniform Bind Group"),
+        });
+
+        let max_buffer_size = 1024 * 1024; // Example: 1MB buffer
+        let vertex_buffer = init.device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Vertex Buffer"),
+            size: max_buffer_size as u64,
+            usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+        let num_vertices = 0;
 
         return (uniform_bind_group, vertex_uniform_buffer, vertex_buffer, num_vertices)
     }
@@ -1157,18 +1166,13 @@ impl State {
             multiview: None
         });
 
-        let mut vertex_buffers: Vec<wgpu::Buffer> = Vec::new();
-        let mut num_vertices: Vec<u32> = Vec::new();
-        let mut uniform_bind_groups: Vec<wgpu::BindGroup> = Vec::new();
-        let mut vertex_uniform_buffers: Vec<wgpu::Buffer> = Vec::new();
+        let (world_uniform_bind_group, world_vertex_uniform_buffer, world_vertex_buffer, world_num_vertices) = 
+            Self::create_world_buffer(&init, light_data, &uniform_bind_group_layout, &world_data);
 
-        for i in 0..game_data.objects.len() {
-            let (uniform_bind_group, vertex_uniform_buffer, vertex_buffer, num_vertices_) = Self::create_object(&game_data, &init, light_data, &uniform_bind_group_layout, i);
-            vertex_buffers.push(vertex_buffer);
-            num_vertices.push(num_vertices_);
-            uniform_bind_groups.push(uniform_bind_group);
-            vertex_uniform_buffers.push(vertex_uniform_buffer);
-        }
+        let vertex_buffers: Vec<wgpu::Buffer> = Vec::new();
+        let num_vertices: Vec<u32> = Vec::new();
+        let uniform_bind_groups: Vec<wgpu::BindGroup> = Vec::new();
+        let vertex_uniform_buffers: Vec<wgpu::Buffer> = Vec::new();
 
         let mut gui_vertex_buffers: Vec<wgpu::Buffer> = Vec::new();
         let mut gui_num_vertices: Vec<u32> = Vec::new();
@@ -1176,7 +1180,8 @@ impl State {
         let mut gui_vertex_uniform_buffers: Vec<wgpu::Buffer> = Vec::new();
 
         for i in 0..game_data.gui_objects.len() {
-            let (uniform_bind_group, vertex_uniform_buffer, vertex_buffer, num_vertices_) = Self::create_object_gui(&game_data, &init, light_data, &uniform_bind_group_layout, i);
+            let (uniform_bind_group, vertex_uniform_buffer, vertex_buffer, num_vertices_) = 
+                Self::create_object_gui(&game_data, &init, light_data, &uniform_bind_group_layout, i);
             gui_vertex_buffers.push(vertex_buffer);
             gui_num_vertices.push(num_vertices_);
             gui_uniform_bind_groups.push(uniform_bind_group);
@@ -1268,19 +1273,23 @@ impl State {
             text_pipeline,
             gui_item_block_pipeline,
             vertex_buffers,
+            world_vertex_buffer,
             gui_vertex_buffers,
             text_vertex_buffers,
             gui_item_block_vertex_buffers,
             uniform_bind_groups,
+            world_uniform_bind_group,
             gui_uniform_bind_groups,
             text_uniform_bind_groups,
             gui_item_block_uniform_bind_groups,
             vertex_uniform_buffers,
+            world_vertex_uniform_buffer,
             gui_vertex_uniform_buffers,
             text_vertex_uniform_buffers,
             gui_item_block_vertex_uniform_buffers,
             project_mat,
             num_vertices,
+            world_num_vertices,
             gui_num_vertices,
             text_num_vertices,
             gui_item_block_num_vertices,
